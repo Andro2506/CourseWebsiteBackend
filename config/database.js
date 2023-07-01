5e2f4f1dd05adb987f2c0e3977b042cfb163723c
Andro2506 <125305336+Andro2506@users.noreply.github.com>
2023-07-01 22:37:36 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI;
+    const uri = "mongodb+srv://sohamdas0625:password1234@cluster0.77c9sbl.mongodb.net/CourseWebsite?retryWrites=true";
     if (!uri) {
       throw new Error("MongoDB connection URI is missing. Make sure to set the MONGO_URI environment variable.");
     }
